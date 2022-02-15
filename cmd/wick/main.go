b5642aa3117f040f2eb49a31eb725f6adf446df3
muzzammilshahid <muzzammilshahid50@gmail.com>
2022-02-15 19:03:34 +0500

    auto select auth method

--- cmd/wick/main.go
+++ cmd/wick/main.go
@@ -84,6 +84,31 @@ func main() {
 	}
 
 	logger := log.New(os.Stdout, "", 0)
+
+	if *privateKey != "" && *ticket != "" {
+		logger.Fatal("Select only one of private key, ticket and secret", "")
+	}
+
+	if *ticket != "" && *secret != "" {
+		logger.Fatal("Select only one of private key, ticket and secret", "")
+	}
+
+	if *privateKey != "" && *secret != "" {
+		logger.Fatal("Select only one of private key, ticket and secret", "")
+	}
+
+	if *privateKey != "" {
+		*authMethod = "cryptosign"
+	}
+
+	if *ticket != "" {
+		*authMethod = "ticket"
+	}
+
+	if *secret != "" {
+		*authMethod = "wampcra"
+	}
+
 	var session *client.Client
 
 	switch *authMethod {
