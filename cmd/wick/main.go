db9328eb6408cb14c2de19868bcc342f77d39b3c
Muzzammil Shahid <muzzammilshahid50@gmail.com>
2022-02-16 23:46:02 +0500

    change Select to Provide

--- cmd/wick/main.go
+++ cmd/wick/main.go
@@ -86,11 +86,11 @@ func main() {
 	logger := log.New(os.Stdout, "", 0)
 
 	if *privateKey != "" && *ticket != "" {
-		logger.Fatal("Select only one of private key, ticket or secret")
+		logger.Fatal("Provide only one of private key, ticket or secret")
 	} else if *ticket != "" && *secret != "" {
-		logger.Fatal("Select only one of private key, ticket or secret")
+		logger.Fatal("Provide only one of private key, ticket or secret")
 	} else if *privateKey != "" && *secret != "" {
-		logger.Fatal("Select only one of private key, ticket or secret")
+		logger.Fatal("Provide only one of private key, ticket or secret")
 	}
 
 	if *privateKey != "" {
