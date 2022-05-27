041d7bdb33c957a947a8d69b3059acd2ec87e6ae
muzzammilshahid <muzzammilshahid50@gmail.com>
2022-05-27 16:26:19 +0500

    add call time

--- wamp/main.go
+++ wamp/main.go
@@ -333,10 +333,15 @@ func Register(session *client.Client, procedure string, command string, delay in
 
 }
 
-func Call(session *client.Client, procedure string, args []string, kwargs map[string]string) {
+func Call(session *client.Client, procedure string, args []string, kwargs map[string]string, timeBool bool) {
 	ctx := context.Background()
 
+	startTime := time.Now().UnixMilli()
 	result, err := session.Call(ctx, procedure, nil, listToWampList(args), dictToWampDict(kwargs), nil)
+	if timeBool {
+		endTime := time.Now().UnixMilli()
+		logger.Println("time taken = ", endTime-startTime)
+	}
 	if err != nil {
 		logger.Println(err)
 	} else if result != nil && len(result.Arguments) > 0 {
